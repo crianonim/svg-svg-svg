f1cbc6fa4da024edcc4b4ee97275bd91877060ac
Jan Skowronski <crianonim@gmail.com>
2019-08-07 07:13:53 +0100

    Polygon

--- index.js
+++ index.js
@@ -2,3 +2,4 @@ const s1=_svg.createSVGElement('svg',{viewBox:"0 0 72 72","class":"svg1"},docume
 _svg.createSVGElement('circle',{cx:20,cy:5,r:11,fill:'pink',stroke:'inherit'},s1);
 _svg.circle(50,50,4,s1);
 _svg.rect(30,20,8,4,s1);
+_svg.polygon("2 4 60 70 50 400",s1);
