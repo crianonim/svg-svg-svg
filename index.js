748cf374de3ed6a71ae9f04131a58058a9d7ccba
Jan Skowronski <crianonim@gmail.com>
2019-08-07 06:56:59 +0100

    Rect

--- index.js
+++ index.js
@@ -1,22 +1,4 @@
 const s1=_svg.createSVGElement('svg',{viewBox:"0 0 72 72","class":"svg1"},document.body);
 _svg.createSVGElement('circle',{cx:20,cy:5,r:11,fill:'pink',stroke:'inherit'},s1);
 _svg.circle(50,50,4,s1);
-// const poly1=document.querySelector('.poly1');
-// const svg=document.querySelector('svg.img1');
-// const p=svg.createSVGPoint();
-// p.x=30;
-// p.y=40;
-// console.log(poly1.points)
-
-// const svg2 = document.createElementNS("http://www.w3.org/2000/svg", "svg");
-
-// svg2.setAttribute('viewBox','0 0 400 400');
-// const circle = document.createElementNS("http://www.w3.org/2000/svg", "circle");
-// circle.setAttribute('cx',"100");
-// circle.setAttribute('cy',"100");
-// circle.setAttribute('r',"50");
-// svg2.appendChild(circle)
-// document.body.appendChild(svg2);
-
-
-// _svg.createSVGElement('rect',{x:300,y:200,width:20,height:40},svg2);
+_svg.rect(30,20,8,4,s1);
